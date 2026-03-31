e32186c30210948d272b805d40f1661a79af981b
ElGabo26 <jcgab2009@gmail.com>
2026-03-31 13:39:05 -0500

    inst5

--- backend/tools/makeContext1.py
+++ backend/tools/makeContext1.py
@@ -293,7 +293,7 @@ def limpiar_respuesta_deepseek(texto: str) -> str:
 # =========================================================
 # PROMPT
 # =========================================================
-with open(r"grupo_danec_split\backend\tools\instrucciones.txt", "r", encoding="utf-8") as f:
+with open(r"\tools\instrucciones.txt", "r", encoding="utf-8") as f:
     instrucciones = f.read()
 
 
