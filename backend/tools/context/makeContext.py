2f121587c831b15166c53db5a0946d5aae279b6a
ElGabo26 <jcgab2009@gmail.com>
2026-04-10 15:11:15 -0500

    v10

--- backend/tools/context/makeContext.py
+++ backend/tools/context/makeContext.py
@@ -111,7 +111,7 @@ def construir_prompt_contexto(contexto: Dict[str, Any], max_columnas_por_tabla: 
 
 def makeContext(question,limit):
     base = Path(".")
-    catalogo = cargar_jsonl("./dominioss_catalogo.jsonl")
+    catalogo = cargar_jsonl("./dominios_catalogo.jsonl")
     dominio0=identificar_dominio(question,catalogo)
     dominio=dominio0["dominio_predicho"]
     if not dominio:
